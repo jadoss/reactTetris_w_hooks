97ef531f84efcfd2a237f4f65ef6137073925867
x <x@xs-MacBook-Pro.local>
2020-04-04 13:42:39 -0500

    Updates

---
+++ precache-manifest.3212856f81801fb4b5b2d695dd6dd983.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e77c0d11facc0b253b3e808fc68af61",
+    "url": "/reactTetris_w_hooks/index.html"
+  },
+  {
+    "revision": "cea3212bd28eb524170a",
+    "url": "/reactTetris_w_hooks/static/css/main.9ae05f73.chunk.css"
+  },
+  {
+    "revision": "eb15f55a429be1539163",
+    "url": "/reactTetris_w_hooks/static/js/2.711471f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactTetris_w_hooks/static/js/2.711471f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea3212bd28eb524170a",
+    "url": "/reactTetris_w_hooks/static/js/main.e463f437.chunk.js"
+  },
+  {
+    "revision": "980104b24af4b8fca95b",
+    "url": "/reactTetris_w_hooks/static/js/runtime-main.ab829699.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/reactTetris_w_hooks/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/reactTetris_w_hooks/static/media/bg.2810fe6b.png"
+  }
+]);
